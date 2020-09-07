0f65fe6221974525c78ac795f37a288efc0b00d8
moaj257 <moaj257@gmail.com>
2020-09-07 22:32:16 +0530

    Added Movie & Movies Pages

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import { useFonts as useInterFonts, Inter_400Regular, Inter_600SemiBold } from '@expo-google-fonts/inter';
+import { useFonts as useOswaldFonts, Oswald_600SemiBold } from '@expo-google-fonts/oswald';
+import Navigation from './Navigation';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default () => {
+  let [interFontsLoaded] = useInterFonts({
+    Inter_400Regular, Inter_600SemiBold
+  });
+
+  let [oswaldFontsLoaded] = useOswaldFonts({
+    Oswald_600SemiBold
+  });
+
+  if (!interFontsLoaded || !oswaldFontsLoaded)
+    return <AppLoading />;
+  else
+    return (
+      <>
+        <StatusBar translucent={false} style='light' />
+        <Navigation />
+      </>
+    )
 }
 
 const styles = StyleSheet.create({
